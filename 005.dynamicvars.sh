884216aa59b40bc2f4a1fd33699e649fb017c8d6
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-11 21:53:07 +0530

    Shell scripting

---
+++ 005.dynamicvars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Example Hardcoding-Not the right approach
+#Date="11/MARCH/2024"
+#No_OF_Sessions="4"
+
+DATE="$(date +%F)"
+No_OF_Sessions="$(who|wc -l)"
+Team=$1
+#Comments
+echo -e "Todays date is ${DATE}"
+
+echo -e "The script name is $0"
+
+echo -e "The team name is ${TEAM}"
