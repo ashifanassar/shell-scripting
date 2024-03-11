ca610b2ea075441b0bec356ff0942421361c2387
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-11 20:59:51 +0530

    Shell scripting

--- 002.escapesequencecharacters.sh
+++ 002.escapesequencecharacters.sh
@@ -7,6 +7,7 @@
 
 echo -e "Line\nLine2"
 echo -e "line3\nline4\tline5"
+echo -e "line6\nline7\nline8\tline9"
 
 #while using special characters always use double quotes ""
 #single quotes : ''-Delimits the power of special characters
