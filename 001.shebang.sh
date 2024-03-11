095be3a4f90462f4413941b546ad9fadabf048a1
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-11 20:16:11 +0530

    Shell scripting

---
+++ 001.shebang.sh
@@ -0,0 +1,14 @@
+#!/bin/shell
+
+#Shebang Notation: This is used to tell which interpreter we are going to use
+#There can only be one interpreter for the script 
+#The notation tells what interpreter that needs to be used
+#other than this if use # they are treated as comments
+#single quotes are dangarous we should use only double codes
+
+sample:
+
+echo "Hello world"
+echo Line1
+echo Line2
+echo welcome to Bash scripting
