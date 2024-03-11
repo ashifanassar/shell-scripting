e965715895b8f5929a22b6a8cc08fd71723e9145
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-11 20:57:26 +0530

    Shell scripting

---
+++ 002.escapesequencecharacters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Escape sequence characters 
+
+#\n is for new line
+#\t is for new tab
+
+echo -e "Line\nLine2"
+echo -e "line3\nline4\tline5"
+
+#while using special characters always use double quotes ""
+#single quotes : ''-Delimits the power of special characters
