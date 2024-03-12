2120114a2f2e20c8d7a8d7cfac85b5b84492f40a
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-12 17:26:07 +0530

    Shell scripting

---
+++ 014.ifelsesyntax.sh
@@ -0,0 +1,52 @@
+#!\bin\bash
+
+<<COMMENT
+
+if conditions are of 3 types
+1.Simple If
+2.If Else
+3.Else If
+
+1) simple IF
+
+if [expression]; then
+
+commands
+
+fi
+
+2) IF ELSE
+
+if [expression]; then
+
+command1
+
+else
+
+command2
+
+fi
+
+3)Else IF
+
+if[expression];then
+
+command
+elif[expression]; then
+
+command
+elif[expression1]; then
+
+command2
+
+elif[expression3]; then
+
+command3
+
+else
+
+command4
+
+fi
+
+COMMENT
