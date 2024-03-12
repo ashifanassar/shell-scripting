6256bedf24e38f7c8045ff0d53659ba4b6b9227c
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-12 17:35:37 +0530

    Shell scripting

--- 015.ifelse.sh
+++ 015.ifelse.sh
@@ -4,15 +4,15 @@ echo -e "Demo of If else, else if condition usage"
 
 ACTION=$1
 
-if["$ACTION" = "start"]; then
+if["$ACTION" = "start"] ; then
     echo -e "Starting the service"
     exit 0
 
-elif["$ACTION" = "stop"]; then
+elif["$ACTION" = "stop"] ; then
     echo -e "stopping the service"
     exit 1
 
-elif["$ACTION" = "restart"]; then
+elif["$ACTION" = "restart"] ; then
     echo -e "restarting the service"
     exit 2
 else
