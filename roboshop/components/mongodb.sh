61a9140cd757af16bb5752ee983faabdb1ccb2c8
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-13 20:07:45 +0530

    Shell scripting

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -36,7 +36,7 @@ systemctl start mongod &>> $LOGFILE
 stat $?
 
 echo -n "Enabling $COMPONENT visibility"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
 echo -n "starting $COMPONENT service"
