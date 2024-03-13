f15a599b66722e48f8932ed2f28758c89d40ce25
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-13 17:56:56 +0530

    Shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,7 +6,7 @@ if [ $ID -ne 0 ]; then
     echo -e "\e[33m Not the root user use the Sudo access \e[0m"
     exit 1
 fi
-
+$COMPONENT="frontend"
 LOGFILE="/tmp/frontend.log"
 
 stat(){
@@ -19,7 +19,7 @@ else
 
 
 echo -n "Installing the Nginx on the server"
-yum install nginx -y  &>> $LOGFILE
+yum install nginx -y  &>> LOGFILE
 stat $?
 
 echo -n "Enabling the service"
@@ -29,5 +29,29 @@ stat $?
 echo -n "starting the service"
 systemctl start nginx &>> LOGFILE
 stat $?
+
+
+echo -n "Downloading the $COMPONENT component"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+echo -n "Cleaning the $COMPONENT cleanup :"
+cd /usr/share/nginx/html
+rm -rf * &>> LOGFILE
+stat $?
+
+echo -n "Extrating $COMPONENT "
+unzip /tmp/frontend.zip &>> LOGFILE
+stat $?
+
+echo -n "configuring $COMPONENT "
+mv ${COMPONENT}-main/* . &>> LOGFILE
+mv static/* . &>> LOGFILE
+rm -rf f${COMPONENT}-master README.md &>> LOGFILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -n "Restarting the service"
+systemctl restart nginx 
+
 
 
