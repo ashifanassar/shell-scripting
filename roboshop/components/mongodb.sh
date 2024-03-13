e1861a247f22760fec11ff8c25413cc8b198dc99
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-13 20:04:50 +0530

    Shell scripting

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,7 +9,7 @@ fi
 
 COMPONENT="mongodb"
 LOGFILE="/tmp/$1.log"
-
+componenturl="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 
 stat(){
     if [ $1 -eq 0 ]; then
@@ -19,18 +19,40 @@ else
     fi
 }
 
-echo "Setup the $COMPONENT"
+echo -n "Setup the $COMPONENT"
 curl -s -0 /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
 
-echo "Install the $COMPONENT"
+echo -n "Install the $COMPONENT"
 yum install -y $COMPONENT-org &>> $LOGFILE
 stat $?
 
-echo "Enable the $COMPONENT"
+echo -n "Enable the $COMPONENT"
 systemctl enable mongod &>> $LOGFILE
 stat $?
 
-echo "Start the $COMPONENT"
+echo -n "Start the $COMPONENT"
 systemctl start mongod &>> $LOGFILE
+stat $?
+
+echo -n "Enabling $COMPONENT visibility"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+stat $?
+
+echo -n "starting $COMPONENT service"
+systemctl restart mongod &>> $LOGFILE
+stat $?
+
+echo -n "Downloading the $COMPONENT schema"
+curl -s -L -o /tmp/$COMPONENT.zip $componenturl &>> $LOGFILE
+stat $?
+
+echo -n "Extracting the $COMPONENT file"
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $?
+
+echo -n "Injecting the  $COMPONENT schema"
+cd /tmp/mongodb-main &>> $LOGFILE
+mongo < catalogue.js
+mongo < users.js 
 stat $?
