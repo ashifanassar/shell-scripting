5569ed1c2311caa5de4e19c0a68716d5faf1e8dc
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-13 17:42:18 +0530

    Shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,20 +2,31 @@
 
 #Check the script is running as a root user or else break the script
 ID=$(id -u)
-if [ $ID -ne 0] ; then
+if [ $ID -ne 0]; then
     echo -e "\e[33m Not the root user use the Sudo access \e[0m"
     exit 1
 fi
 
+LOGFILE="/tmp/frontend.log"
 
-echo "Installing the Nginx on the sderver"
-yum install nginx -y
+stat(){
+    if [ $1 -eq 0 ]; then
+    echo -e "\e[32m success\e[0m"
+else
+    echo -e "\e[32m failure\e[0m"
+}
 
-echo "Enabling the service"
-systemctl enable nginx 
 
-echo "starting the service"
-systemctl start nginx 
+echo -n "Installing the Nginx on the server"
+yum install nginx -y  &>> $LOGFILE
+stat $?
 
+echo -n"Enabling the service"
+systemctl enable nginx &>> LOGFILE
+stat $?
+
+echo -n "starting the service"
+systemctl start nginx &>> LOGFILE
+stat $?
 
 
