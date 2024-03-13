0e3ffaed2cf8fbc729900df4a51e87b2cb17a99f
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-13 17:24:04 +0530

    Shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Check the script is running as a root user or else break the script
+ID=$(id -u)
+if [ $ID -ne 0] ; then
+    echo -e "\e[33m Not the root user use the Sudo access \e[0m"
+    exit 1
+fi
+
 
 echo "Installing the Nginx on the sderver"
 yum install nginx -y
