675c6da4b2069e0b3dfccf2eaf77b3d853c3f012
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-14 17:43:14 +0530

    Shell scripting

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -24,12 +24,13 @@ dnf install $REDIS_REPO -y &>> $LOGFILE
 stat $?
 
 echo -e "Enabling $COMPONENT"
-dnf module enable redis:remi 6.2 -y &>> $LOGFILE
+dnf module enable redis:remi-6.2 -y  &>>  $LOGFILE
 stat $?
 
 echo -e "Installing $COMPONENT"
 dnf install redis -y &>> $LOGFILE
 stat $?
+
 
 echo -e "Enabling the $COMPONENT visibility"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
