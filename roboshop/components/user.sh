056e444d7a3b6e0fa7156063bb4423e90e29cc75
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-15 19:30:10 +0530

    Shell scripting

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -3,12 +3,5 @@
 source components/common.sh  #Source has all the funtions
 
 COMPONENT="user"
-LOGFILE="/tmp/$COMPONENT.log"
-APPUSER="roboshop"
-APP_DIR="/home/roboshop/$COMPONENT"
-
-
 
 Nodejs() #check in the common.sh file
-
-
