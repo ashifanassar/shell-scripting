8138f44e4b60e2bf1bc93a6764ed299bfb855769
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-15 17:19:11 +0530

    Shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
-#Check the script is running as a root user or else break the script
-
-source components/common.sh  #Source has all the funtions
+# Checks whether you're running this script with sudo or a root user or not.If not, exit the script
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m This script is expected to run with sudo or as a root user \e[0m   \n\t Ex:  bash scriptName compName"
+    exit 1
+fi 
 
 COMPONENT="frontend"
 LOGFILE="/tmp/$1.log"
 
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo "\e[31m Failure \e[0m"
+    fi 
+}
 
-
-echo -n "Installing the Nginx on the server"
-yum install nginx -y  &>> $LOGFILE
+echo -e "\e[35m **************** ______ $COMPONENT Configuration Is Started ______  **************** \e[0m"
+echo -n "Intalling Nginx Web Server :"
+dnf install nginx -y        &>>  $LOGFILE
 stat $?
 
-echo -n "Enabling the service"
-systemctl enable nginx &>> $LOGFILE
+echo -n "Enabling the service :"
+systemctl enable nginx      &>>  $LOGFILE
 stat $?
 
-echo -n "starting the service"
-systemctl start nginx &>> $LOGFILE
-stat $?
-
-
-echo -n "Downloading the $COMPONENT component"
+echo -n "Downloading the $COMPONENT Component: "
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
+stat $? 
 
-echo -n "Cleaning the $COMPONENT cleanup :"
-cd /usr/share/nginx/html
-rm -rf * &>> LOGFILE
-stat $?
+echo -n "Performing $COMPONENT Cleanup :"
+cd /usr/share/nginx/html 
+rm -rf * &>>  $LOGFILE
+stat $? 
 
-echo -n "Extrating $COMPONENT "
-unzip /tmp/frontend.zip &>> $LOGFILE
-stat $?
+echo -n "Extracting $COMPONENT :"
+unzip /tmp/frontend.zip   &>>  $LOGFILE 
+stat $? 
 
-echo -n "configuring $COMPONENT "
-mv ${COMPONENT}-main/* . &>> $LOGFILE
-mv static/* . &>> $LOGFILE
-rm -rf f${COMPONENT}-master README.md &>> $LOGFILE
+echo -n "Configuring $COMPONENT :"
+mv ${COMPONENT}-main/* .   &>>  $LOGFILE
+mv static/* .        &>>  $LOGFILE
+rm -rf ${COMPONENT}-main README.md    &>>  $LOGFILE  
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
-
 
 echo -n "Updating Reverse Proxy File: "
     for i in catalogue user cart ; do 
@@ -49,9 +53,8 @@ echo -n "Updating Reverse Proxy File: "
     done
 stat $?
 
-
-echo -n "Restarting the service"
-systemctl restart nginx 
+echo -n "Retarting the Web Server: "
+systemctl restart nginx       &>>  $LOGFILE
 stat $?
 
-
+echo -e "\e[35m ****** ______ $COMPONENT Configuration Is Completed ****** ______ \e[0m"
