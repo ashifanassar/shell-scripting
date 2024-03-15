2787b0be2109014f9cad1ed571d04eb51a5e9442
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-15 20:14:54 +0530

    Shell scripting

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -8,16 +8,3 @@ COMPONENT="cart"
 
 
 NODEJS #check in the common.sh file
-
-CREATE_USER  #check in the common.sh file
-
-
-DOWNLOAD_EXTRACT
-
-
-CONFIG_SVC()
-
-START_SVC()
-
-echo -n "status of the $COMPONENT"
-systemctl start $COMPONENT
