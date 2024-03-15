a6a6cbcf8a210010a2505d699a44945ca102cf6f
Ashifa Nassar <ashifa.nassar@hcl.com>
2024-03-15 19:44:55 +0530

    Shell scripting

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -51,7 +51,7 @@ DOWNLOAD_AND_EXTRACT() {
 CONFIG_SVC() {
     echo -n "Configuring Permissions :"
     mv /home/roboshop/${COMPONENT}-main ${APPUSER_DIR} &>>  $LOGFILE
-    chown -R ${APPUSER}:${APPUSER} ${APPUSER_DIR}      &>>  $LOGFILE
+    chown ${APPUSER}:${APPUSER} ${APPUSER_DIR}      &>>  $LOGFILE
     stat $? 
 
     echo -n "Configuring $COMPONENT Service: "
