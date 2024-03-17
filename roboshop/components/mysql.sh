e9bd9985ddfd78044524d711a37cc5c904448c89
ashifanassar <ashifanassar@gmail.com>
2024-03-17 12:08:49 +0530

    Shell scripting

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -32,7 +32,7 @@ stat $?
 
 if [ $? -ne 0 ]; then 
     echo -n "Performing default password reset of root account:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
     stat $?
 fi 
 
