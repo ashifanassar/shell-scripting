18f6d83cdfe2e321508ce71808607b1b637c0f04
ashifanassar <ashifanassar@gmail.com>
2024-03-18 16:50:54 +0530

    Shell scripting

--- roboshop/launchec2.sh
+++ roboshop/launchec2.sh
@@ -4,22 +4,46 @@
 AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-0ea84c81ce84682ff"    
 COMPONENT=$1
-HOSTED_ZONE=Z0547409165EGAKUG3EH3
+HOSTED_ZONE="Z0547409165EGAKUG3EH3"
+COLOR="\e[37m]"
+NOCOLOUR="\e[0m"
+ENV=$2
 
 
-if [ -z $1 ] ; then
+if [ -z $1 ] || [ -z $2]; then
     echo -e "\e[31m   COMPONENT NAME IS NEEDED: \e[0m"
     echo -e "\e[36m \t\t Example Usage : \e[0m  bash launch-ec2 ratings"
     exit 1
 fi 
 
 
-#
-PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .Instances[].PrivateIpAddress |sed -e 's/"//g')
-echo "$1 Server Created and here is the IP ADDRESS $PRIVATE_IP"
+if [ -x $1 ] ; then
+    echo -e "\e[31m component name is needed \e[0m"
+    echo -e "\e[36m \t example usage \e[0m"
+    exit 1
+fi
+
+
+
+
+EC2_FUN()
+{
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}-${ENV}}]" | jq .Instances[].PrivateIpAddress |sed -e 's/"//g')
+echo "$COLOR $1-$2 Server Created and here is the IP ADDRESS $PRIVATE_IP $NOCOLOR"
 
 echo "Creating r53 json file with component name and ip address:"
-sed -e "s/IPADDRESS/${PRIVATE_IP}/g" -e "s/COMPONENT/${COMPONENT}/g" route53.json > /tmp/dns.json
+sed -e "s/IPADDRESS/${PRIVATE_IP}/g" -e "s/COMPONENT/${COMPONENT}-${ENV}/g" route53.json > /tmp/dns.json
 
 echo "Creating DNS Record for $COMPONENT :"
 aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE} --change-batch file:///tmp/dns.json 
+}
+
+#To create the components
+if [$1 = all] ; then
+    for x in frontend mongodb catalogue user redis cart mysql shipping rabbitmq payment; do
+    COMPONENT=$x
+    EC2_FUN
+    done
+else
+    EC2_FUN
+fi
